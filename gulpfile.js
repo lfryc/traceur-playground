83bd7f5ba21c030193e311b92adb2258a84e0bf5
Lukas Fryc <lukas@fryc.eu>
2014-09-10 09:52:16 +0200

    gulp watch + livereload

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,12 @@ gulp.task('default', ['traceur']);
 
 gulp.task('traceur', function () {
   return gulp.src(paths.scripts)
-    .pipe(traceur({sourceMap: true, modules: 'amd', experimental: true}))
+    .pipe(traceur({sourceMaps: true, modules: 'amd', experimental: true}))
     .pipe(gulp.dest('dist'))
-    .pipe(livereload())
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['traceur']);
+  livereload.listen();
+  gulp.watch(paths.scripts, ['traceur']).on('change', livereload.changed);
 });
