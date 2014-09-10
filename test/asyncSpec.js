2be2ae692fa518f110323b9918ccdb9357f18f5d
Lukas Fryc <lukas@fryc.eu>
2014-09-10 09:25:31 +0200

    upgraded to Jasmine 2 for simplification of async tests

--- test/asyncSpec.js
+++ test/asyncSpec.js
@@ -2,17 +2,11 @@ import {asyncValue} from '../src/async.js';
 
 describe('Async', function () {
 
-    it('should add', function () {
-        var done = false;
-        runs(function() {
-            (async function() {
-                var value = await asyncValue(42).catch(console.error.bind(console));
-                expect(value).toEqual(42);
-                done = true;
-            })();
-        });
-        waitsFor(function() {
-            return done;
-        });
+    it('run async', function (done) {
+        (async function() {
+            var value = await asyncValue(42).catch(console.error.bind(console));
+            expect(value).toEqual(42);
+            done();
+        })();
     });
 });
