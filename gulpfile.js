73864329ef285dd1ba7740e1b73d24ea9657e59e
Lukas Fryc <lukas@fryc.eu>
2014-09-10 10:27:57 +0200

    added karma tests run to default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 var gulp = require('gulp');
 var traceur = require('gulp-traceur');
 var livereload = require('gulp-livereload');
+var karma = require('karma').server;
 
 var paths = {
   scripts: ['src/**.js']
 };
 
-gulp.task('default', ['traceur']);
+gulp.task('default', ['traceur', 'test']);
 
 gulp.task('traceur', function () {
   return gulp.src(paths.scripts)
     .pipe(traceur({sourceMaps: true, modules: 'amd', experimental: true}))
     .pipe(gulp.dest('dist'))
+});
+
+gulp.task('test', function(done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
 });
 
 // Rerun the task when a file changes
